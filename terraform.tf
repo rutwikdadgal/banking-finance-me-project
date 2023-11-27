149f714ad39b8cd8ce5d03e444d9015018d2932b
Rutwik <rutwikdadgal2001@gmail.com>
2023-11-27 10:36:35 +0530

    terraform.tf added

--- terraform.tf
+++ terraform.tf
@@ -34,9 +34,9 @@ resource "null_resource" "configure-ansible-hosts" {
 
   provisioner "local-exec" {
     command = <<EOT
-      sudo mkdir -p /etc/ansible
-      echo "${aws_instance.test-server.public_ip}" >> /etc/ansible/hosts
-      echo "${aws_instance.prod-server.public_ip}" >> /etc/ansible/hosts
+      sudo mkdir  /etc/ansible
+      echo "${aws_instance.test-server.public_ip}" | sudo tee -a /etc/ansible/hosts
+      echo "${aws_instance.prod-server.public_ip}" | sudo tee -a /etc/ansible/hosts
     EOT
   }
 }
