b3a79ca70fe548f16f5ee1dd52b33cc3bd788011
Rutwik <rutwikdadgal2001@gmail.com>
2023-11-27 08:36:08 +0530

    terraform.tf added

---
+++ terraform.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.0"  
+    }
+  }
+}
+
+provider "aws" {
+  region     = "ap-southeast-1"  
+}
+
+resource "aws_instance" "test-server" {
+  ami           = "ami-0287a05f0ef0e9d9a (64-bit (x86))"  
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "test-server"
+  }
+  provisioner = "exec local" {
+  	command = " echo ${aws_instance.test-server.public_ip} >> /etc/ansible/hosts"
+  	}
+  
+resource "aws_instance" "prod-server" {
+  ami           = ""  
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "prod-server"
+  }
+   provisioner = "exec local" {
+  	command = " echo ${aws_instance.prod-server.public_ip} >> /etc/ansible/hosts"
+  	}
+}
