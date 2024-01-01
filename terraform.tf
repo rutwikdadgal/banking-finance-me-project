442f701014f352ada3dabfbacdf80544c0608c6a
Rutwik <rutwikdadgal2001@gmail.com>
2024-01-01 11:44:27 +0530

    terraform updated

--- terraform.tf
+++ terraform.tf
@@ -10,12 +10,59 @@ terraform {
 provider "aws" {
   region = "ap-south-1"  
 }
+ 
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "main_subnet" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow-ssh"
+  description = "Allow inbound SSH traffic"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow-ssh"
+  }
+}
 
 resource "aws_instance" "test_server" {
   ami           = "ami-02a2af70a66af6dfb"
   instance_type = "t2.micro"
   key_name      = "devops-key"
   
+  subnet_id = aws_subnet.main_subnet.id
+
   tags = {
     Name = "test-server"
   }
@@ -25,6 +72,8 @@ resource "aws_instance" "prod_server" {
   ami           = "ami-02a2af70a66af6dfb"  
   instance_type = "t2.micro"
   key_name      = "devops-key"
+  subnet_id     = aws_subnet.main_subnet.id
+
   tags = {
     Name = "prod-server"
   }
@@ -37,3 +86,4 @@ output "test_server_ip" {
 output "prod_server_ip" {
   value = aws_instance.prod_server.public_ip
 }
+
