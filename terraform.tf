a19fe286777a0329dfa93396af74f6b1645fd207
Rutwik <rutwikdadgal2001@gmail.com>
2024-01-01 11:55:41 +0530

    terraform updated

--- terraform.tf
+++ terraform.tf
@@ -27,6 +27,8 @@ resource "aws_subnet" "main_subnet" {
   vpc_id     = aws_vpc.main_vpc.id
   cidr_block = "10.0.1.0/24"
 
+  availability_zone = "ap-south-1a"  # Added availability_zone parameter
+
   tags = {
     Name = "Main"
   }
